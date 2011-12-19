8b44d84cb94e4d520daf62f3267808462a685527
Baptiste Mathus <batmat@batmat.net>
2011-12-19 16:11:06 +0100

    Project simple init after archetype:generate

---
+++ src/main/java/net/batmat/hsearchissue/App.java
@@ -0,0 +1,13 @@
+package net.batmat.hsearchissue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
